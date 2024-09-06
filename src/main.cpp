b15cc907a4a551f54dc45ae4123b1ef1f3c0e832
Richke2005 <richardke18@gmail.com>
2024-09-05 23:43:50 -0300

    making some tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,25 @@
 #include <Arduino.h>
 #include <config.hpp>
 #include <hBridgeMotor.hpp>
+#include <SoftwareSerial.h>
 // put function declarations here:
+const byte TX = 4;
+const byte RX = 5;
+
 HBridgeMotor bridge1(PIN1A, PIN2A);
+SoftwareSerial bluetooth(TX, RX);
 
 void setup() {
   // put your setup code here, to run once:
   bridge1.initialize();
   Serial.begin(9600);
+  bluetooth.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if(Serial.read() == 'a'){
-  	bridge1.setMotorA(true, 1);
+    bridge1.setMotorA((float) 0.0, 1);
   }
   if(Serial.read() == 'i')
     bridge1.setMotorA(true, 2);
